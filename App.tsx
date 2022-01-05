b537517abe9c59cc0b079a4bedc84526f4398104
Michel Ange SHERFAN <manonchazalet@macbook-air-de-manon.home>
2022-01-05 20:14:43 +0100

    Setup app architecture.

--- App.tsx
+++ App.tsx
@@ -1,114 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import * as React from 'react';
+import {StyleSheet, View, StatusBar, Platform, Text} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SafeAreaView from 'react-native-safe-area-view';
+import {WebView} from 'react-native-webview';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaProvider>
+      <SafeAreaView
+        forceInset={{top: 'always', bottom: 'never'}}
+        style={Platform.OS === 'ios' ? styles.StatusBarForIos : null}>
+        {Platform.OS === 'android' ? (
+          <StatusBar backgroundColor={'#007fcb'} />
+        ) : (
+          StatusBar.setBarStyle('light-content', true)
+        )}
+        <Text>test</Text>
+        <View style={styles.Container}>
+          <WebView source={{uri: 'https://google.com/'}} />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  StatusBarForIos: {
+    height: StatusBar.currentHeight,
+    backgroundColor: '#007fcb',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  Container: {
+    borderWidth: 1,
+    // borderColor: '#007fcb',
+    borderColor: 'red',
+    width: '100%',
+    height: '100%',
   },
 });
 
