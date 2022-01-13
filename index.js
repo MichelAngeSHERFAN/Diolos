74691a4d52bcee11a4460f24c0acb2b9f00c9435
Michel Ange SHERFAN <manonchazalet@MacBook-Air-de-Manon.local>
2022-01-13 19:21:04 +0100

    Added notification systhem on ios.

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, Platform } from "react-native";
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName.toLowerCase(), () => App);
+if (Platform.OS === 'ios') {
+  AppRegistry.registerComponent(appName, () => App);
+} else {
+  AppRegistry.registerComponent(appName.toLowerCase(), () => App);
+}
+
+// AppRegistry.registerComponent(appName.toLowerCase(), () => App);
